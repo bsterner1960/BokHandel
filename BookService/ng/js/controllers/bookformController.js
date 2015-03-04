79a9239159f6b4c4f4dfdbd40d462a3c81346f5e
Hugo Leandersson <hugo.leandersson@telia.com>
2015-03-04 11:26:28 +0100

    Added form interaction logic to bookformController, also getting author model using Authors service

--- BookService/ng/js/controllers/bookformController.js
+++ BookService/ng/js/controllers/bookformController.js
@@ -1,3 +1,21 @@
 ﻿app.controller("bookformController", ["$scope", "Books", "Authors", function ($scope, Books, Authors) {
     console.log("bookformController is alive! Wohooooo :D");
+
+    //first make a call to get all authors
+    Authors.get();
+    //listen for the data to come back
+    $scope.$on("restSuccess", function (event, data) {
+        console.log("restSuccess: ", data);
+        $scope.authors = data;
+    });
+
+    //the book model to be sent to Database
+    $scope.bookData = {};
+
+    //click handler for form
+    $scope.save = function () {
+        Books.post($scope.bookData);
+    };
+
+
 }]);
