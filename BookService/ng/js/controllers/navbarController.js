1347394ad2bbfb59e150d649e558cc745fced845
Lora <Lora.Twinblade@gmail.com>
2015-03-14 00:33:47 +0100

    More work on the Navbar, it should now be quite functional, but not yet complete.
    Still lacking proper communication between other views, also need to
    look into a better radio button alternative.

--- BookService/ng/js/controllers/navbarController.js
+++ BookService/ng/js/controllers/navbarController.js
@@ -1,8 +1,5 @@
-﻿var app = angular.module('Muffins', ['ui.bootstrap']);
-
-
-
-app.controller('DropdownCtrl', function ($scope)
+﻿angular.module('ui.bootstrap').controller('Muffins', function ($scope)
 {
-    // There is no code needed here, yet this app.controller is still needed to make the navbar work, *mind-blown*
+    console.log("navbarController is alive! Wohooooo :D");
+    $scope.minTest = "hejsan!"
 });
