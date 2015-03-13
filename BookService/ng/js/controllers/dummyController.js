fcb3ad05eefadc841f1f26923bc3225a27943819
Lora <Lora.Twinblade@gmail.com>
2015-03-13 20:22:25 +0100

    Finally fixed the Navbar

---
+++ BookService/ng/js/controllers/dummyController.js
@@ -0,0 +1,8 @@
+﻿var app = angular.module('Muffins', ['ui.bootstrap']);
+
+
+
+app.controller('DropdownCtrl', function ($scope)
+{
+    // There is no code needed here, yet this app.controller is still needed to make the navbar work, *mind-blown*
+});
