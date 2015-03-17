353fc4403d086db0d46533938d2d0d730e74d8b2
bsterner1960 <bsterner1960@gmail.com>
2015-03-17 09:05:01 +0100

    bok och DTO klass o DB pågår

--- BookService/Migrations/Configuration.cs
+++ BookService/Migrations/Configuration.cs
@@ -4,68 +4,28 @@ namespace BookService.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
-    using BookService.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<BookService.Models.BookServiceContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(BookService.Models.BookServiceContext context)
         {
-            context.Authors.AddOrUpdate(x => x.Id,
-                new Author() { Id = 1, Name = "Jane Austen" },
-                new Author() { Id = 2, Name = "Charles Dickens" },
-                new Author() { Id = 3, Name = "Miguel de Cervantes" }
-                );
+            //  This method will be called after migrating to the latest version.
 
-            context.Genres.AddOrUpdate(x => x.Id,
-                new Genre() { Id = 1, Name = "Comedy of manners" },
-                new Genre() { Id = 2, Name = "Gothic parody" },
-                new Genre() { Id = 3, Name = "Bildungsroman" },
-                new Genre() { Id = 4, Name = "Picaresque" }
-                );
-
-            context.Books.AddOrUpdate(x => x.Id,
-                new Book()
-                {
-                    Id = 1,
-                    Title = "Pride and Prejudice",
-                    Year = 1813,
-                    AuthorId = 1,
-                    Price = 9.99M,
-                    GenreId = 1
-                },
-                new Book()
-                {
-                    Id = 2,
-                    Title = "Northanger Abbey",
-                    Year = 1817,
-                    AuthorId = 1,
-                    Price = 12.95M,
-                    GenreId = 2
-                },
-                new Book()
-                {
-                    Id = 3,
-                    Title = "David Copperfield",
-                    Year = 1850,
-                    AuthorId = 2,
-                    Price = 15,
-                    GenreId = 3
-                },
-                new Book()
-                {
-                    Id = 4,
-                    Title = "Don Quixote",
-                    Year = 1617,
-                    AuthorId = 3,
-                    Price = 8.95M,
-                    GenreId = 4
-                }
-                );
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
         }
     }
 }
---
+++ BookService/Models/BookGenreConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookService.Models
+{
+    public class BookGenreConnection
+    
+    {
+        public int Id { get; set; }
+        [Required]
+
+        // Foreign Key
+        public int BookId { get; set; }
+       // public Book Book { get; set; } 
+       
+        // Foreign Key
+        public int GenreId { get; set; }
+        //public Genre Genre { get; set; }  
+
+
+    }
+}
