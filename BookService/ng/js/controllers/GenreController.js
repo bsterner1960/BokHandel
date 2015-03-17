c7472e8c5f922d69cd00041cfa527be7e81b42c6
AndersGn <graneman@yahoo.se>
2015-03-17 09:46:03 +0100

    Tog bort konflikt "kod"

--- BookService/ng/js/controllers/GenreController.js
+++ BookService/ng/js/controllers/GenreController.js
@@ -1,17 +1,9 @@
 ﻿app.controller("genreController", ["$scope", "Genres","$routeParams", function ($scope, Genres, $routeParams) {
 
-<<<<<<< HEAD
-    console.log("genre controllern är igång!", $routeParams);
-
-    //$scope.genres = Genres.index();
-    $scope.jonas = Genres.show($routeParams);
-     
-=======
     console.log("genre controllern är igång!");
 
     //$scope.genres = Genres.index();
     $scope.jonas = Genres.show({ id: 1 });
 
->>>>>>> origin/master
 }]);
 
