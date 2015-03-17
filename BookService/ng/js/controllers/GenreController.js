913754bd599ec5369c4f7d08c5d32525bbcea0e2
AndersGn <graneman@yahoo.se>
2015-03-17 15:39:03 +0100

    VY Controller för genre detaljer (skapad av Jonas)
    Har varit inne och hackat lite.

--- BookService/ng/js/controllers/GenreController.js
+++ BookService/ng/js/controllers/GenreController.js
@@ -1,21 +1,24 @@
 ﻿app.controller("genreController", ["$scope", "Genres", "$routeParams", "$location", function ($scope, Genres, $routeParams, $location) {
 
     console.log("genre controllern är igång! routeParams är nu: ", $routeParams);
-//if ($routeParams.id.indexOf("Genre")) {
-        //$location.url("/");
-    //}
-
+    if (!$routeParams || !$routeParams.id) {
+        $location.url("/");
+    }
 
     //$scope.genres = Genres.index();
-    $scope.jonas = Genres.show({ id: 1 });
+    $scope.genre = Genres.show($routeParams);
   
 
-    $scope.options = ["Genre1", "Genre2", "Genre3"];
+    ////$scope.options = ["Genre1", "Genre2", "Genre3"];
+    ////$scope.options = [Genres.show({id:6})];
+    //$scope.options = [Genres.show({ id: 1 })];
 
-    $scope.madeSelection = function (selectedIndex) {
-        alert("user is going to route: " + "/genredetails/" + $scope.options[selectedIndex]);
-        $location.url("/genredetails/" + $scope.options[selectedIndex]);
-    }
+    //$scope.madeSelection = function (selectedIndex) {
+    //    //alert("user is going to route: " + "/genredetails/" + $scope.options[selectedIndex]);
+    //    alert("user is going to route: " + "/genredetails/" + $scope.options[selectedIndex].Id);
+    //    console.log($scope.options[selectedIndex].Id)
+    //    $location.url("/genredetails/" + $scope.options[selectedIndex].Id);
+    //}
 
 }]);
 
