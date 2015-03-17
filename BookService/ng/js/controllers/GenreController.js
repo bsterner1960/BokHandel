5dda0586ecfd254f28719df215507099bb7f756c
AndersGn <graneman@yahoo.se>
2015-03-17 09:42:34 +0100

    Tog bort konflikt "kod"

--- BookService/ng/js/controllers/GenreController.js
+++ BookService/ng/js/controllers/GenreController.js
@@ -1,18 +1,9 @@
 ﻿app.controller("genreController", ["$scope", "Genres", function ($scope, Genres) {
 
     console.log("genre controllern är igång!");
-<<<<<<< HEAD
 
     //$scope.genres = Genres.index();
     $scope.jonas = Genres.show({ id: 1 });
 
-
-  
-=======
-    
-    //$scope.genres = Genres.index();
-    $scope.jonas = Genres.show({ id: 1 });
->>>>>>> origin/master
- 
 }]);
 
