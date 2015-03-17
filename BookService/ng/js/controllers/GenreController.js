02ea088b17811b7f07f8bb49346cc188db30868e
Jonas Bergkvist <letifer0007@hotmail.com>
2015-03-17 08:49:05 +0100

    genre details
    uppdaterat

--- BookService/ng/js/controllers/GenreController.js
+++ BookService/ng/js/controllers/GenreController.js
@@ -1,9 +1,9 @@
 ﻿app.controller("genreController", ["$scope", "Genres", function ($scope, Genres) {
 
     console.log("genre controllern är igång!");
-  
+    
     //$scope.genres = Genres.index();
-    $scope.genres = Genres.index();
+    $scope.jonas = Genres.show({ id: 1 });
 
   
  
