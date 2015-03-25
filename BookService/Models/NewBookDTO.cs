9323587c384871292f0efadda9ee999ba3c76516
bsterner1960 <bsterner1960@gmail.com>
2015-03-25 11:46:52 +0100

    NewBookDTO

---
+++ BookService/Models/NewBookDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+namespace BookService.Models
+{
+    public class NewBookDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public int StockBalance { get; set; }
+
+        public List<int> AuthorIds { get; set; }
+        public List<int> GenreIds { get; set; }
+
+    }
+}
