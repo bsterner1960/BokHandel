d7db47ff804d20ab33b8feb125645deef09576c7
bsterner1960 <bsterner1960@gmail.com>
2015-04-02 11:23:22 +0200

    städat bort testkod Rest-anrop

--- BookService/ng/js/controllers/homeController.js
+++ BookService/ng/js/controllers/homeController.js
@@ -14,26 +14,26 @@ app.controller("homeController", ["$scope", "$rootScope", "restService", "Books"
     //*                             TESTING CODE!                             *
     //*************************************************************************
 
-    app.service('browser', ['$window', function ($window) {
+    //app.service('browser', ['$window', function ($window) {
 
-        return function () {
+    //    return function () {
 
-            var userAgent = $window.navigator.userAgent;
+    //        var userAgent = $window.navigator.userAgent;
 
-            var browsers = { chrome: /chrome/i, safari: /safari/i, firefox: /firefox/i, ie: /internet explorer/i };
+    //        var browsers = { chrome: /chrome/i, safari: /safari/i, firefox: /firefox/i, ie: /internet explorer/i };
 
-            for (var key in browsers) {
-                if (browsers[key].test(userAgent)) {
-                    $scope.alerts.push({ type: 'danger', msg: "Web browser found: " + key });
+    //        for (var key in browsers) {
+    //            if (browsers[key].test(userAgent)) {
+    //                $scope.alerts.push({ type: 'danger', msg: "Web browser found: " + key });
 
-                    return key;
-                }
-            };
+    //                return key;
+    //            }
+    //        };
 
-            return 'unknown';
-        }
+    //        return 'unknown';
+    //    }
 
-    }]);
+    //}]);
 
     //*************************************************************************
     //*                         END OF TESTING CODE!                          *
@@ -41,48 +41,48 @@ app.controller("homeController", ["$scope", "$rootScope", "restService", "Books"
 
 
 
-    $rootScope.bookSearchValue = "";
+    //$rootScope.bookSearchValue = "";
 
-    //CRUD (POST, GET, PUT, DELETE)
-    $scope.restMethods = ["GET", "PUT", "POST", "DELETE"];
+    ////CRUD (POST, GET, PUT, DELETE)
+    //$scope.restMethods = ["GET", "PUT", "POST", "DELETE"];
 
-    //click handler for our form in the vie
-    $scope.restCall = function () {
-        //data is a string (value of textarea)
-        var data = $scope.callData;
+    ////click handler for our form in the vie
+    //$scope.restCall = function () {
+    //    //data is a string (value of textarea)
+    //    var data = $scope.callData;
 
-        //if user did not pick a url && method, abort
-        if (!$scope.callUrl || !$scope.callMethod) {
-            // if something is missing, notify user
-            $scope.restMessage = "Please fill in all required fields!";
-            return;
-        }
+    //    //if user did not pick a url && method, abort
+    //    if (!$scope.callUrl || !$scope.callMethod) {
+    //        // if something is missing, notify user
+    //        $scope.restMessage = "Please fill in all required fields!";
+    //        return;
+    //    }
 
-        //send call and recieve true || false from restService method
-        var callSent = restService.restCall($scope.callUrl, $scope.callMethod, data);
+    //    //send call and recieve true || false from restService method
+    //    var callSent = restService.restCall($scope.callUrl, $scope.callMethod, data);
 
-        //if callSent === true the request was sent with valid data
-        if (callSent) {
-            $scope.restMessage = false;
-        } else {
-            //else something must have been wrong with the data
-            $scope.restMessage = "Bad request data!";
-        }
+    //    //if callSent === true the request was sent with valid data
+    //    if (callSent) {
+    //        $scope.restMessage = false;
+    //    } else {
+    //        //else something must have been wrong with the data
+    //        $scope.restMessage = "Bad request data!";
+    //    }
 
-    };
+    //};
 
-    //we can make models accessible to the entire app using $rootScope!
-    //$scope.hello is set in restService helloWorld using $rootScope
-    ////console.log("rootScope1: ", $scope.hello);
-    //restService.helloWorld();
-    ////console.log("rootScope2: ", $scope.hello);
+    ////we can make models accessible to the entire app using $rootScope!
+    ////$scope.hello is set in restService helloWorld using $rootScope
+    //////console.log("rootScope1: ", $scope.hello);
+    ////restService.helloWorld();
+    //////console.log("rootScope2: ", $scope.hello);
 
-    //a static GET request to see if our service is alive
-    //restService.restCall("books", "GET", {});
-    //replacing restService with Books (also a service)
-    Books.get();
+    ////a static GET request to see if our service is alive
+    ////restService.restCall("books", "GET", {});
+    ////replacing restService with Books (also a service)
+    //Books.get();
 
-    $scope.goTo = function (url) {
-        $location.url(url);
-    }
+    //$scope.goTo = function (url) {
+    //    $location.url(url);
+    //}
 }]);
